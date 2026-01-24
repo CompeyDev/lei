d0a071e21c560aa578b058e9796ba0bfb7c723a2
Erica Marigold <hi@devcomp.xyz>
2026-01-24 11:02:05 +0000

    refactor(lua/number): use value receivers for `LuaNumber`

--- lua/number.go
+++ lua/number.go
@@ -12,10 +12,10 @@ var _ LuaValue = (*LuaNumber)(nil)
 
 // Numbers are cheap to copy, so we don't store the reference index
 
-func (n *LuaNumber) lua() *Lua { return nil }
-func (n *LuaNumber) ref() int  { return ffi.LUA_NOREF }
-func (n *LuaNumber) deref(lua *Lua) int {
+func (n LuaNumber) lua() *Lua { return nil }
+func (n LuaNumber) ref() int  { return ffi.LUA_NOREF }
+func (n LuaNumber) deref(lua *Lua) int {
 	state := lua.state()
-	ffi.PushNumber(state, ffi.LuaNumber(*n))
+	ffi.PushNumber(state, ffi.LuaNumber(n))
 	return int(ffi.GetTop(state))
 }
